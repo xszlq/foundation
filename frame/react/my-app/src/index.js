312d103f8ce2a2cc11bcdb4e16e0c770e481e3b8
LiuQiang <xszlq@foxmail.com>
2018-04-15 15:06:57 +0800

    add redux dir

---
+++ frame/react/my-app/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+//import { createLogger } from 'redux-logger'
+
+import reducer from './pages/redux-async/reducers'
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const middleware = [ thunk ];
+
+const store = createStore(
+    reducer,
+    applyMiddleware(...middleware)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+
+registerServiceWorker();
