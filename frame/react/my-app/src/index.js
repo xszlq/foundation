685fb23e8425fc8d06c65dacd3d822333ae61945
LiuQiang <xszlq@foxmail.com>
2018-04-25 22:15:06 +0800

    middleware

--- frame/react/my-app/src/index.js
+++ frame/react/my-app/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-//import { createLogger } from 'redux-logger'
+import { logger  } from 'redux-logger'
 
 import reducer from './pages/redux-async/reducers'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const middleware = [ thunk ];
+const middleware = [ thunk, logger ];
 
 const store = createStore(
     reducer,
