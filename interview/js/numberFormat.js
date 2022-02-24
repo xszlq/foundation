ae3a5f0725172ae557aa927d3efb798e2ce02d4c
liuqiang <qiang.liu@dmall.com>
2022-02-24 10:17:21 +0800

    feat: reg version

--- interview/js/numberFormat.js
+++ interview/js/numberFormat.js
@@ -29,17 +29,11 @@ function numberFormat(num){
 
 // 正则版本
 function regVerison(num){
-    const [integer, float] = num.toString().split(".");
-    console.log(typeof integer)
-
-    // let addComma = integer.replace(/\d{1-3}(?=(\d{3})+&)/g, '$1,');
-    let addComma = integer.replace(/(\d)(?=(\d{3})+(?!\d))/g,'$1,');
-
-    return addComma + (float ? '.' + float : '')
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
 console.log(numberFormat(153812.7))
 console.log(numberFormat(12.7))
 console.log(numberFormat(24323412.7))
 
-console.log(regVerison(1343253812.7))
+console.log(regVerison(1343253812.23))
