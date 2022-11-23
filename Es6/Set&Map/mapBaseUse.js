f0c632d329b3af042baea5e532ed860aaa68cfdf
liuqiang <liuqiang02@g7.com.cn>
2022-11-23 10:14:30 +0800

    feat: set map

--- Es6/Set&Map/mapBaseUse.js
+++ Es6/Set&Map/mapBaseUse.js
@@ -1,5 +1,5 @@
 const o1 = {},
-    o2 = {};
+  o2 = {};
 
 const map1 = new Map();
 
@@ -11,6 +11,23 @@ console.log(map1.get(o1));
 /*
     构造函数
  */
-const map2 = new Map([[o1, "ao1"], [o2, "ao2"]]);
+const map2 = new Map([
+  [o1, "ao1"],
+  [o2, "ao2"],
+]);
 
 console.log(map2.get(o2));
+
+// 1.map实例属性和方法
+console.log(map2.size);
+
+map2.clear();
+console.log(map2.size);
+
+map2.set("a", "valueA");
+map2.set("b", "valueB");
+
+// 2.遍历map
+for (let i of map2) {
+  console.log(i); // ['a', 'valueA'], ['b', 'valueB']
+}
--- Es6/Set&Map/setUsage.js
+++ Es6/Set&Map/setUsage.js
@@ -1,6 +1,27 @@
 /*
     数组去重
  */
-const a1 = [1,1,2,3,3,5];
+const a1 = [1, 1, 2, 3, 3, 5];
 
 console.log([...new Set(a1)]);
+
+// 1. set 实例属性 & 方法
+const s1 = new Set();
+
+const obj = { obj: "obj1" };
+
+s1.add(1);
+s1.add(2);
+s1.add(1);
+s1.add(obj);
+
+console.log(s1.size); // 3
+
+s1.delete(2);
+
+console.log(s1.size); // 2
+
+// 2. 遍历set
+for (let i of s1) {
+  console.log(i);
+}
